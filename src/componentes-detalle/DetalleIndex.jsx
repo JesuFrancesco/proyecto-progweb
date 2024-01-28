2dcb52731b1ee48811bfc065abce62d9fa9c5146
Ghost5x9 <103043680+Ghost5x9@users.noreply.github.com>
2024-01-28 14:15:04 -0500

    Update DetalleIndex.jsx

--- src/componentes-detalle/DetalleIndex.jsx
+++ src/componentes-detalle/DetalleIndex.jsx
@@ -37,13 +37,13 @@ const DetalleIndex = () => {
 
   return (
     <div className="row contenido">
-      <div style={{ borderBottom: "solid 2px rgb(196, 196, 196)", width: "960px" }}>
-        <Typography variant="h1" className="mt-3" style={{ fontFamily: "Roboto", fontSize: "34px" }}>
-          Película
+      <div style={{ borderBottom: "solid 2px rgb(196, 196, 196)", width: "1180px" }}>
+        <Typography variant="h1" className="mt-3" style={{ fontFamily: "Roboto", fontSize: "45px" }}>
+          Películas
         </Typography>
       </div>
       <div className="mt-4 titu">
-        <div style={{ fontSize: "34px", fontFamily: "Roboto" }}>{peliculaActual.titulo}</div>
+        <div style={{ fontSize: "40px", fontFamily: "Roboto" }}>{peliculaActual.titulo}</div>
         <div className="mb-3">
           <div className="ubi">
             <img src={ubi} alt="ubicacion" />
@@ -57,17 +57,17 @@ const DetalleIndex = () => {
         </div>
       </div>
       <div className="col sinopsis" style={{ paddingLeft: "30px" }}>
-        <div className="card" style={{ width: "546px", height: "286px" }}>
-          <iframe height="286" src={peliculaActual.trailer} allowFullScreen />
+        <div className="card" style={{ width: "700px", height: "400px" }}>
+          <iframe height="400" src={peliculaActual.trailer} allowFullScreen />
         </div>
       </div>
-      <div className="col">
-        <div className="card" style={{ width: "290px", height: "284px" }}>
+      <div className="col-4">
+        <div className="card" style={{ width: "300px", height: "400px" }}>
           <div className="card-body">
-            <Typography variant="h5" className="card-title mb-3" style={{ fontSize: "24px" }}>
+            <Typography variant="h5" className="card-title mb-3" style={{ fontSize: "30px" }}>
               Sinopsis
             </Typography>
-            <Typography variant="body1" className="card-text" style={{ fontSize: "16px", fontFamily: "Roboto", marginBottom: "15px" }}>
+            <Typography variant="body1" className="card-text" style={{ fontSize: "20px", fontFamily: "Roboto", marginBottom: "15px" }}>
               La brutal campaña de venganza de un hombre adquiere dimensiones nacionales cuando se descubre que es un antiguo agente de una poderosa organización clandestina conocida como "Los apicultores"
             </Typography>
             <Chip label={peliculaActual.etiqueta1} />
@@ -78,7 +78,7 @@ const DetalleIndex = () => {
       </div>
       <p></p>
       <div style={{ paddingLeft: "30px" }}>
-        <Typography variant="h2" style={{ fontSize: "34px", fontFamily: "Roboto" }}>
+        <Typography variant="h2" style={{ fontSize: "45px", fontFamily: "Roboto" }}>
           Salas disponibles
         </Typography>
       </div>
@@ -92,4 +92,3 @@ const DetalleIndex = () => {
 };
 
 export default DetalleIndex;
-
