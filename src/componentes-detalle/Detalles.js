e1ceb406a84b4a1e591227c3b7998a50207cd154
Ghost5x9 <103043680+Ghost5x9@users.noreply.github.com>
2024-01-27 22:39:59 -0500

    Add files via upload

---
+++ src/componentes-detalle/Detalles.js
@@ -0,0 +1,17 @@
+export const Salas_dispo = [
+  {abrevia: "S1",sala: "Sala A",descripcion:  "It is a long established fact that a reader will be distracted by the readable content of a page when looking at itslayout",horarios: ["15:00", "17:00"]},
+  {abrevia: "S2",sala: "Sala B",descripcion:  "It is a long established fact that a reader will be distracted by the readable content of a page when looking at itslayout",horarios: ["16:00", "18:00"]},
+  {abrevia: "S3",sala: "Sala C",descripcion:  "It is a long established fact that a reader will be distracted by the readable content of a page when looking at itslayout",horarios: ["20:00", "22:00"]},
+];
+
+export const peliculas = [
+  {peliHora: "1 hrs 50 min", peliName: "Beekeper Sentencia de Muerte", peliEtiqueta: ["Acción", "+14"], trailer: "https://www.youtube.com/embed/J2pWkhP3ou0", director: "David Ayer"},
+  { peliHora: "1 hrs 20 min", peliName: "La super Familia", peliEtiqueta: ["Familiar", "APT"], trailer: "https://www.youtube.com/embed/6GJYfU1dt5w", director: "Rasmus A. Sivertsen"},
+  { peliHora: "2 hrs 10 min", peliName: "Aquaman y el Reino Perdido", peliEtiqueta: ["Acción", "+14"], trailer: "https://www.youtube.com/embed/M8W_y0mmJEc", director: "James Wan"},
+  { peliHora: "2 hrs 10 min", peliName: "El Niño y la Garza", peliEtiqueta: ["Anime", "APT"], trailer: "https://www.youtube.com/embed/PW7vSgGGElA", director: "Hayao Miyazaki"},
+  { peliHora: "1 hrs 40 min", peliName: "Soul", peliEtiqueta: ["Animación", "APT"], trailer: "https://www.youtube.com/embed/3QIdlo4uIVg", director: "Pete Docter"},
+  { peliHora: "1 hrs 40 min", peliName: "El juego de la Muerte", peliEtiqueta: ["Terror", "+14"], trailer: "https://www.youtube.com/embed/6t_jjM1aHOE", director: "Anna Zaytseva"},
+  { peliHora: "1 hrs 40 min", peliName: "Jack en la Caja Maldita 3", peliEtiqueta: ["Terror", "+14"], trailer: "https://www.youtube.com/embed/0Xp2_FoXjQ8", director: "Lawrence Fowler"},
+  { peliHora: "1 hrs 50 min", peliName: "Alice: La Gemela del Diablo", peliEtiqueta: ["Terror", "+14"], trailer: "https://www.youtube.com/embed/ObSYc9_o6Uo", director: "William Mark McCullough"},
+  { peliHora: "1 hrs", peliName: "Hablando Huevadas: Hijo D...", peliEtiqueta: ["Comedia", "+14"], trailer: "https://www.youtube.com/embed/7pZGLnGJAJs", director: "Chino Pinto"}
+];
