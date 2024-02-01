4e3e916ba4603ca1ace3152204dfe381c5524ea6
Ghost5x9 <103043680+Ghost5x9@users.noreply.github.com>
2024-01-31 19:34:07 -0500

    Update DetalleIndex.jsx

--- src/componentes-detalle/DetalleIndex.jsx
+++ src/componentes-detalle/DetalleIndex.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {useParams } from "react-router-dom";
+import {useLocation, useParams } from "react-router-dom";
 import Typography from "@mui/material/Typography";
 import DetalleImagen from "./DetalleImagen";
 import DetalleTitulo from "./DetalleTitulo";
@@ -7,51 +7,62 @@ import DetalleSala from "./DetalleSala";
 import DetalleSinopsis from "./DetalleSinopsis";
 
 const DetalleIndex = () => {
+  const ruta = useLocation();
+  console.log("--- DetalleIndex.jsx")
+  console.log(ruta.state.usuario_obj)
+  
   const errorComponent = <>
     <img src="https://http.cat/images/404.jpg" alt="" />
   </>
 
-  const location = useParams();
-  const [peliculaActual, setPeliculaActual] = useState({
-    id: "",
-    path: "",
-    titulo: "",
-    year: "",
-    cast: [],
-    trailer: "",
-    extract: "",
-    generos: [],
-    url: []
-  });
-
+const movie = useParams();
+console.log("pelicula: ")
+console.log(movie.id)
+const [peliculaActual, setPeliculaActual] = useState({
+  id: "",
+  path: "",
+  titulo: "",
+  year: "",
+  cast: [],
+  trailer: "",
+  extract: "",
+  generos: [],
+  url: []
+});
   const [error, setError] = useState(false);
   
-  const buscarPeliculasHTTP = async () => {
-    const response = await fetch("https://raw.githubusercontent.com/ulima-pw/data-20240/main/peliculas_limpio.json");
-    const peliculas = await response.json();
-    const path = location.id
-    
-    const peliculaActual = peliculas.find((pelicula) => pelicula.path === path);
-    if(peliculaActual){
-      setPeliculaActual({
-        id: peliculaActual.id,
-        path: peliculaActual.path,
-        titulo: peliculaActual.title,
-        year: peliculaActual.year,
-        cast: peliculaActual.cast,
-        trailer: peliculaActual.thumbnail,
-        extract: peliculaActual.extract,
-        generos: peliculaActual.genres,
-        url: peliculaActual.thumbnail
-      })
-    } else {
-      setError(true);
-    }
-  }  
+  
   // Buscar peliculas...
   useEffect(() => {
+    const buscarPeliculasHTTP = async () => {
+      const response = await fetch("https://raw.githubusercontent.com/ulima-pw/data-20240/main/peliculas_limpio.json");
+      const peliculas = await response.json();
+      const path = movie.id
+  
+      // const rutaActual = location.pathname;
+      // const path = rutaActual.split("/")[2]
+  
+      // console.log(path.id)
+      
+      const peliculaActual = peliculas.find((pelicula) => pelicula.path === path);
+      if(peliculaActual){
+        setPeliculaActual({
+          id: peliculaActual.id,
+          path: peliculaActual.path,
+          titulo: peliculaActual.title,
+          year: peliculaActual.year,
+          cast: peliculaActual.cast,
+          trailer: peliculaActual.thumbnail,
+          extract: peliculaActual.extract,
+          generos: peliculaActual.genres,
+          url: peliculaActual.thumbnail
+        })
+      } else {
+        setError(true);
+      }
+    }
     buscarPeliculasHTTP();
-  }, [location.id]);
+  }, [movie]);
 
   if(error === true){
     return errorComponent
