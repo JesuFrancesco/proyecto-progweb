3af9e7b82f049bd03d7874c390f1eee4dce4d293
Jesu <117495071+JesuFrancesco@users.noreply.github.com>
2024-02-01 18:54:10 -0500

    fix: fondo de Loginpage

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -7,7 +7,7 @@ import { useNavigate } from 'react-router-dom'
 const LoginPage = () => {
   const navegar = useNavigate()
   useEffect(() => {
-    document.body.classList.remove("fondo-body")
+    // document.body.classList.remove("fondo-body")
     if(checkLoginInForm()){
       navegar("/menu")
     }
