d10f7758aecc6d17c4b4e466ffd6146410cbedd2
Jesu <117495071+JesuFrancesco@users.noreply.github.com>
2024-02-01 18:47:55 -0500

    fix: redireccion cuando se encuentra logeado

--- src/util/CheckLogin.jsx
+++ src/util/CheckLogin.jsx
@@ -1,7 +1,13 @@
-export function checkLogin() {
-    
+export function checkLogin() {    
     if (!sessionStorage.getItem('usuario_objeto')) { 
         return false;
     }
     return true;
+}
+export function checkLoginInForm() {
+    
+    if (sessionStorage.getItem('usuario_objeto')) { 
+        return true;
+    }
+    return false;
 }
