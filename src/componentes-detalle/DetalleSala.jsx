374b844307c3d99a1422e4f70e630f0a161f0fa1
Jesu <117495071+JesuFrancesco@users.noreply.github.com>
2024-02-01 18:36:13 -0500

    feat: integracion de detalle con salas de ulima

--- src/componentes-detalle/DetalleSala.jsx
+++ src/componentes-detalle/DetalleSala.jsx
@@ -4,17 +4,24 @@ import { useEffect, useState } from "react";
 
 const Salas = (props) => {
   const [salas, setSalas] = useState([]);
+  const [salasUL, setSalasUL] = useState([]);
   const [contadorSala] = useState(1);
 
   const obtenerSalas = async () => {
       const response = await fetch("https://raw.githubusercontent.com/ulima-pw/data-20240/main/salas.json");
       const data = await response.json();
       setSalas(data.buildings);
-      
+  };
+
+  const obtenerSalasUL = async () => {
+      const response = await fetch("https://raw.githubusercontent.com/JesuFrancesco/proyecto-progweb/main/public/salas.json");
+      const data = await response.json();
+      setSalasUL(data);
   };
 
   useEffect(() => {
     obtenerSalas();
+    obtenerSalasUL();
   }, []);
 
   return (
@@ -29,6 +36,18 @@ const Salas = (props) => {
           key={index}
           sala={detalle.name}
           horarios={detalle.available_times}
+          titulo={props.titulo}
+          id={props.path}
+          imagen={props.url}
+          usuario_obj7={props.usuario_obj6}
+        />
+      ))}
+      {salasUL.map((detalle, index) => (
+        <Detalle
+          abrevia={`UL${contadorSala + index}`}
+          key={index}
+          sala={detalle.salaName}
+          horarios={detalle.salaTimes}
           titulo={props.titulo}
           id={props.path}
           imagen={props.url}
