0a568bcded05b2431af36f73cb572e2976ac418a
Jesu <117495071+JesuFrancesco@users.noreply.github.com>
2024-02-02 08:04:31 -0500

    fix: param en Menu.jsx

--- src/MenuPage.jsx
+++ src/MenuPage.jsx
@@ -53,7 +53,7 @@ const MenuPage = () => {
         <Box display="flex" justifyContent="center">
           <Stack spacing={14} direction="row">
 
-              <Link to={"/peliculas-index"}>
+              <Link to={"/peliculas-index/1"}>
                 <Button
                   variant="contained" style={{ backgroundColor: "#FA7900", fontSize: '16px', color: 'white', width: '7rem' }}>
                   Películas
