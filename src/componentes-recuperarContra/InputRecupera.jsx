0c041ae376ad137f75fdc7a302821a94d0c23c57
Jesu <117495071+JesuFrancesco@users.noreply.github.com>
2024-02-02 19:06:52 -0500

    style: estilo para recuperacion de contraseña

--- src/componentes-recuperarContra/InputRecupera.jsx
+++ src/componentes-recuperarContra/InputRecupera.jsx
@@ -1,6 +1,18 @@
-import { TextField } from '@mui/material'
-
+import { TextField, createTheme } from '@mui/material'
+import { ThemeProvider } from '@mui/material'
 const InputRecupera = (props) => {
+
+    const tema = createTheme({
+        palette: {
+            primary: {
+                main: '#FF5733',
+            },
+            secondary: {
+                main: '#fafafa',
+            },
+        },
+    })
+
     const handleInputChange = (event) => {
         const nuevoObj = props.objeto
         nuevoObj[props.llave] = event.target.value;
@@ -11,7 +23,12 @@ const InputRecupera = (props) => {
     return <>
     <div className="mb-3">
         <div className="form-label">{props.title}</div>
-        <TextField type={props.variante} fullWidth label={props.title} value={props.valor} variant="outlined" onChange={ handleInputChange } />
+        <ThemeProvider theme={tema}>
+            <TextField type={props.variante} 
+            fullWidth 
+            value={props.valor} 
+            variant="outlined" onChange={ handleInputChange } sx={{ '& legend': { display: 'none' }, '& fieldset': { top: 0 },}} />
+        </ThemeProvider>
     </div><div className="mb-3"></div>
     </>
 }
