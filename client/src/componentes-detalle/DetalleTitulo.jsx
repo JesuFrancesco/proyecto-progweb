2aff88371ec6f4350929b076c15602197cca3c9e
Ghost5x9 <103043680+Ghost5x9@users.noreply.github.com>
2024-02-22 17:35:58 -0500

    Update DetalleTitulo.jsx

--- client/src/componentes-detalle/DetalleTitulo.jsx
+++ client/src/componentes-detalle/DetalleTitulo.jsx
@@ -3,20 +3,24 @@ import PersonIcon from '@mui/icons-material/Person';
 import { Link} from "react-router-dom";
 
 const Pelicula = (props) => {
-    return <div className="mt-4 titu">
-    <div className="peli">
-    <div style={{ fontSize: "40px", fontFamily: "Roboto" }}>{props.titulo}</div>
-      <div className="mb-3">
-        <div className="ubi">
-        <DateRangeIcon />
-        <Link style={ {marginRight: "1em"} }>&nbsp; {props.year}</Link>
-
-        <PersonIcon />
-      {/*<Link>&nbsp; {props.cast[0]}</Link>*/}
+  return (
+    <div className="mt-4 titu">
+      <div className="peli">
+        <div style={{ fontSize: "40px", fontFamily: "Roboto" }}>{props.titulo}</div>
+        <div className="mb-3">
+          <div className="ubi">
+            <DateRangeIcon />
+            <Link style={{ marginRight: "1em" }}>&nbsp; {props.year}</Link>
+            {props.cast.map((person, index) => (
+              <Link key={index}>
+                <PersonIcon /> {person} &nbsp; &nbsp;
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  );
 }
 
 export default Pelicula
