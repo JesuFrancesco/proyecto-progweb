0014f26692349ec0202638cbb5d3e52e2765f90d
Ghost5x9 <103043680+Ghost5x9@users.noreply.github.com>
2024-02-25 14:28:20 -0500

    Update DetalleSinopsis.jsx

--- client/src/componentes-detalle/DetalleSinopsis.jsx
+++ client/src/componentes-detalle/DetalleSinopsis.jsx
@@ -1,15 +1,16 @@
 import Chip from "@mui/material/Chip";
 import Typography from "@mui/material/Typography";
+import { color } from "@mui/system";
 const DetalleSinopsis = (props) => {
     return  <div className="col-md">
         <div className="card" style={{ marginLeft: "130px"}}>
             <div className="card-body">
-                <Typography variant="h5" className="card-title mb-3" style={{ fontSize: "30px" }}>
+                <Typography variant="h5" className="card-title mb-3" style={{ fontSize: "30px" }} color={"text.primary"}>
                 Sinopsis
                 </Typography>
 
                 {/* la sinopsis */}
-                <Typography variant="body1" className="card-text" style={{ fontSize: "20px", fontFamily: "Roboto", marginBottom: "15px" }}>
+                <Typography variant="body1" className="card-text" style={{ fontSize: "20px", fontFamily: "Roboto", marginBottom: "15px"}} color={"text.primary"}>
                 {
                     props.extracto
                 }
